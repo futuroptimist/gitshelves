121fdbf280cd2bb282ad123f6c0de2c5786a5173
futuroptimist <80668851+futuroptimist@users.noreply.github.com>
2025-07-09 23:45:45 -0700

    Add monthly grid generation

--- gitshelves/__init__.py
+++ gitshelves/__init__.py
@@ -1,6 +1,11 @@
 """Utilities for generating 3D printable GitHub contribution charts."""
 
-from .scad import generate_scad, blocks_for_contributions
+from .scad import generate_scad, generate_scad_monthly, blocks_for_contributions
 from .fetch import fetch_user_contributions
 
-__all__ = ["generate_scad", "blocks_for_contributions", "fetch_user_contributions"]
+__all__ = [
+    "generate_scad",
+    "generate_scad_monthly",
+    "blocks_for_contributions",
+    "fetch_user_contributions",
+]
